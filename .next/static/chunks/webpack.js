a93b4d674d59ac4e6e5ff5df2a0a659805f7c43e
Oisamaye1 <ovibenz@gmail.com>
2025-04-29 01:34:06 +0300

    subscription route fix3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a64220c325aa6f6a"; }
+/******/ 		__webpack_require__.h = function() { return "ce9ef76137c92f44"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
